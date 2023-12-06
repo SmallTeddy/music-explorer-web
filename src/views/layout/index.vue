fe291982f1f59ebb43e46d6a240a5c87104259e4
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-06 16:20:48 +0800

    fix: delete not useful file

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -4,12 +4,9 @@
   <div class="common-layout">
     <el-container>
       <el-header class="flex-c flex-align header">
-        <h2 style="color: #ffffff90">SmallTeddy</h2>
+        <h2 style="color: #ffffff90">SmallTeddy Music</h2>
       </el-header>
       <el-container>
-        <!-- <el-aside class="flex-c flex-align h-100 aside">
-          Aside
-        </el-aside> -->
         <el-main>
           <router-view />
         </el-main>
@@ -22,11 +19,6 @@
 .header {
   height: 60px;
   background-color: #333;
-}
-
-.aside {
-  background-color: #d3dce6;
-  width: 200px;
 }
 
 .el-main {

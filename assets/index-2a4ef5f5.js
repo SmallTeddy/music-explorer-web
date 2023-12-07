91306bda17eaafc99d968cfc093e1ae3c7e2ee3a
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-07 12:15:15 +0800

    Updates

---
+++ assets/index-2a4ef5f5.js
@@ -0,0 +1,80 @@
+import{d as O,r as s,c as S,o as J,a as b,b as Y,w as h,g as G,e as k,f as C,h as a,u as L,i as K,t as z,j as A,k as Q,F as X,p as Z,l as ee,_ as te}from"./index-340378d4.js";const ne=""+new URL("十一月的肖邦-5bd2859e.png",import.meta.url).href,ae=""+new URL("夜曲-e1816606.mp3",import.meta.url).href,B=p=>(Z("data-v-04bd4773"),p=p(),ee(),p),le=B(()=>a("h2",{style:{color:"#ffffff90"}},"SmallTeddy Music",-1)),se={class:"music-container flex flex-column flex-align"},oe={class:"audio-container flex-align"},ce=B(()=>a("img",{src:ne,alt:"十一月的肖邦",style:{width:"5.2rem"}},null,-1)),ie=B(()=>a("div",{class:"flex-column",style:{"margin-left":"0.5rem"}},[a("div",{style:{"font-size":"1.6rem",color:"#999","margin-bottom":"0.2rem"}},"夜曲"),a("div",{style:{"font-size":"1.2rem",color:"#999"}},"周杰伦 - 夜曲")],-1)),ue={class:"mp3Box"},re=["src"],ve={style:{width:"60px","text-align":"center"}},de={ref:"containerRef",class:"lrc-container"},fe={ref:"ulRef",class:"lrc-list"},_e={class:"cvs-container"},me={ref:"canvasRef"},he=O({__name:"index",setup(p){const{proxy:y}=G(),D=s(),v=s([]),m=s(null),d=s(null),T=s(null),f=s(null),l=s(null),g=s(!1),o=s(null),c=s(null),x=s("Wavy"),E=()=>{D.value=`[00:00.000]夜曲 - 周杰伦 (Jay Chou)
+[00:04.990]词：方文山
+[00:09.980]曲：周杰伦
+[00:14.970]编曲：林迈可
+[00:19.970]制作人：周杰伦
+[00:24.960]一群嗜血的蚂蚁 被腐肉所吸引
+[00:27.960]我面无表情 看孤独的风景
+[00:30.980]失去你 爱恨开始分明
+[00:33.470]失去你 还有什么事好关心
+[00:36.430]当鸽子不再象征和平
+[00:38.310]我终于被提醒
+[00:39.680]广场上喂食的是秃鹰
+[00:41.990]我用漂亮的押韵
+[00:43.170]形容被掠夺一空的爱情
+[00:45.500]
+[00:46.950]啊 乌云开始遮蔽 夜色不干净
+[00:49.640]公园里 葬礼的回音 在漫天飞行
+[00:52.400]送你的白色玫瑰
+[00:54.010]在纯黑的环境凋零
+[00:55.550]乌鸦在树枝上诡异的很安静
+[00:57.890]静静听 我黑色的大衣
+[01:00.340]想温暖你日渐冰冷的回忆
+[01:02.390]走过的走过的生命
+[01:03.970]啊 四周弥漫雾气
+[01:05.180]啊 我在空旷的墓地
+[01:06.570]老去后还爱你
+[01:08.040]
+[01:08.980]为你弹奏肖邦的夜曲
+[01:12.080]纪念我死去的爱情
+[01:14.370]
+[01:14.890]跟夜风一样的声音
+[01:17.560]心碎的很好听
+[01:20.380]手在键盘敲很轻
+[01:23.120]我给的思念很小心
+[01:25.890]你埋葬的地方叫幽冥
+[01:29.590]
+[01:30.100]为你弹奏肖邦的夜曲
+[01:34.160]纪念我死去的爱情
+[01:36.970]而我为你隐姓埋名
+[01:39.660]在月光下弹琴
+[01:42.480]对你心跳的感应
+[01:45.200]还是如此温热亲近
+[01:47.920]怀念你那鲜红的唇印
+[01:52.050]
+[02:15.190]那些断翅的蜻蜓 散落在这森林
+[02:18.340]而我的眼睛 没有丝毫同情
+[02:21.420]失去你 泪水混浊不清
+[02:23.810]失去你 我连笑容都有阴影
+[02:26.900]风在长满青苔的屋顶
+[02:28.670]嘲笑我的伤心
+[02:30.060]像一口没有水的枯井
+[02:32.300]我用凄美的字型
+[02:33.370]描绘后悔莫及的那爱情
+[02:36.220]
+[02:37.280]为你弹奏肖邦的夜曲
+[02:40.330]纪念我死去的爱情
+[02:43.100]跟夜风一样的声音
+[02:45.820]心碎的很好听
+[02:48.740]手在键盘敲很轻
+[02:51.360]我给的思念很小心
+[02:54.150]你埋葬的地方叫幽冥
+[02:57.660]
+[02:58.500]为你弹奏肖邦的夜曲
+[03:02.390]纪念我死去的爱情
+[03:05.210]而我为你隐姓埋名
+[03:07.920]在月光下弹琴
+[03:10.760]对你心跳的感应
+[03:13.420]还是如此温热亲近
+[03:16.240]怀念你那鲜红的唇印
+[03:20.370]
+[03:21.370]一群嗜血的蚂蚁 被腐肉所吸引
+[03:24.490]我面无表情 看孤独的风景
+[03:27.520]失去你 爱恨开始分明
+[03:29.990]失去你 还有什么事好关心
+[03:32.980]当鸽子不再象征和平
+[03:34.810]我终于被提醒
+[03:36.240]广场上喂食的是秃鹰
+[03:38.470]我用漂亮的押韵
+[03:39.680]形容被掠夺一空的爱情`,M()},M=()=>{const e=D.value.split(`
+`);v.value=[],e.map(t=>{const n=t.split("]"),u=n[0].substring(1),i={time:N(u),words:n[1]};v.value.push(i)})},N=e=>{const t=e.split(":");return+t[0]*60+ +t[1]},q=S(()=>{var e;return(e=T.value)==null?void 0:e.clientHeight}),F=S(()=>{var e;return(e=d.value.children[0])==null?void 0:e.clientHeight}),H=S(()=>{var e;return((e=d.value)==null?void 0:e.clientHeight)-q.value}),$=()=>{const e=m.value.currentTime;for(let t=0;t<v.value.length;t++)if(e<v.value[t].time)return t-1;return v.value.length-1},U=()=>{const e=$();let t=F.value*e+F.value/2-q.value/2;t=t<0?0:t,t=t>H.value?H.value:t,d.value.style.transform=`translateY(-${t}px)`;let n=d.value.querySelector(".active");n&&n.classList.remove("active"),n=d.value.children[e],n&&n.classList.add("active")},P=()=>{f.value.width=window.innerWidth*devicePixelRatio,f.value.height=300},V=()=>{m.value.onplay=()=>{if(g.value)return;const e=new AudioContext,t=e.createMediaElementSource(m.value);o.value=e.createAnalyser(),o.value.fftSize=512,c.value=new Uint8Array(o.value.frequencyBinCount),t.connect(o.value),o.value.connect(e.destination),g.value=!0}},I=()=>{requestAnimationFrame(I);const{width:e,height:t}=f.value;if(l.value.clearRect(0,0,e,t),!!g.value&&c.value){o.value&&o.value.getByteFrequencyData(c.value);const n=c.value.length,u=e/n;l.value.fillStyle="skyblue",c.value.forEach((i,_)=>{const r=i/255*t,W=_*u,w=t-r;l.value.fillRect(W,w,u,r)})}},R=()=>{requestAnimationFrame(R);const{width:e,height:t}=f.value;if(l.value.clearRect(0,0,e,t),!!g.value&&c.value){o.value&&o.value.getByteFrequencyData(c.value);const n=c.value.length,u=e*1/n;let i=0;l.value.beginPath(),l.value.strokeStyle="skyblue",c.value.forEach((_,r)=>{const w=_/255*t/2;r===0?l.value.moveTo(i,w):l.value.lineTo(i,w),i+=u}),l.value.lineTo(e,t/2),l.value.stroke()}},j=()=>{x.value=x.value==="Columnar"?"Wavy":"Columnar",x.value=="Columnar"?I():R()};return J(()=>{E(),m.value=y.$refs.audioRef,d.value=y.$refs.ulRef,T.value=y.$refs.containerRef,f.value=y.$refs.canvasRef,l.value=f.value.getContext("2d"),V(),P(),R(),m.value.addEventListener("timeupdate",U)}),(e,t)=>{const n=b("el-header"),u=b("el-button"),i=b("el-main"),_=b("el-container");return k(),Y(_,null,{default:h(()=>[C(n,{class:"flex-c flex-align header"},{default:h(()=>[le]),_:1}),C(_,null,{default:h(()=>[C(i,null,{default:h(()=>[a("div",se,[a("div",oe,[ce,ie,a("div",ue,[a("audio",{ref:"audioRef",controls:"",src:L(ae)},null,8,re)]),a("div",ve,[C(u,{link:"",onClick:j},{default:h(()=>[K(z(L(x)),1)]),_:1})])]),a("div",de,[a("ul",fe,[(k(!0),A(X,null,Q(L(v),r=>(k(),A("li",{key:r.time},z(r.words),1))),128))],512)],512),a("div",_e,[a("canvas",me,null,512)])])]),_:1})]),_:1})]),_:1})}}});const ye=te(he,[["__scopeId","data-v-04bd4773"]]);export{ye as default};

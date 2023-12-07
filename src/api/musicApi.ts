0a1f66be92ecc32af19e2cfb8317c84be876dbaa
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-07 12:07:41 +0800

    Updates

--- src/api/musicApi.ts
+++
@@ -1,9 +0,0 @@
-import testRequest from '@/utils/service'
-import { TEST_BASE_URL } from '@/utils/service/config'
-
-// 登陆
-export function getLrc() {
-  return testRequest.get({
-    url: `${TEST_BASE_URL}/music`
-  })
-}
--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { constantRoutes } from './route'
-
-const router = createRouter({
-  history: createWebHistory('/music-explorer-web'),
-  routes: constantRoutes,
-})
-
-export default router
